b2ca8ed9e79ecb2df56ea899104da03f0e09e8d4
Kolhal Shuklesh Sanjay <104990863+Shuklesh4449@users.noreply.github.com>
2023-09-04 06:36:07 +0530

    Add files via upload

---
+++ PositiveOrNegative.java
@@ -0,0 +1,16 @@
+package com.java.intro;
+
+public class PositiveOrNegative {
+	public static void main (String [] args)
+	{
+		int a= -12 ;
+		//Using if-else statement to check the No is Positive or Negative
+		if (a>0)
+			System.out.println("The Number is Positive");
+		else if(a<0)
+			System.out.println ("The Number is Negative");
+		else
+			System.out.println("Zero");	
+	}
+
+}
