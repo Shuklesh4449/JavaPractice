0aa7f6f78ed3447793957539e4b3f15ead15ac9e
Kolhal Shuklesh Sanjay <104990863+Shuklesh4449@users.noreply.github.com>
2023-09-04 06:37:40 +0530

    Add files via upload

---
+++ NaturalNo.java
@@ -0,0 +1,15 @@
+package com.java.intro.ex;
+
+public class NaturalNo {
+	public static void main(String[] args)
+	{
+		int i, number = 10, sum=0;
+		
+		for(i=1; i<= number;i++)
+		{
+			sum = sum+i;
+		}
+		System.out.println("The sumof First 10 Natural no is :"+sum);
+	}
+
+}
