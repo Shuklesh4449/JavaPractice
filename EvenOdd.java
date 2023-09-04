347231e190eeff3eeb9022936d362e3f91de222b
Kolhal Shuklesh Sanjay <104990863+Shuklesh4449@users.noreply.github.com>
2023-09-04 06:35:24 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,12 @@
+package com.java.intro;
+
+public class EvenOdd {
+	public static void main(String[] args) {
+	int number = 22;
+	//checking whether the number is even or odd
+	if (number % 2 == 0)
+		System.out.println(number + " is Even");
+	else
+	    System.out.println(number + " is odd");
+	 }
+}
