618c1924bc946b4dd577349b5718a719273873bb
Kolhal Shuklesh Sanjay <104990863+Shuklesh4449@users.noreply.github.com>
2023-09-04 06:39:55 +0530

    Add files via upload

---
+++ celsiusToFahrenheit.java
@@ -0,0 +1,12 @@
+package com.java.intro.ex;
+
+public class celsiusToFahrenheit {
+	public static void main(String[] args)
+	{
+		float celsius=15, Fahrenheit;
+		
+		Fahrenheit=((celsius*9)/5)+32;
+		System.out.println("Fahrenheit is:"+Fahrenheit);
+	}
+
+}
